e6ce1f760f2e5a7e2b4f0e5d70670c7dfd561acf
Daniel Peshevski <Daniel.Peshevski@seavus.com>
2019-05-22 22:41:16 +0200

    Lecture 4 #1 Adds the index routing and fs

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import helmet from 'helmet';
 import logger from 'morgan';
 import cors from 'cors';
 
-import users from './users/index'
+import indexRouter from './index/router';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(indexRouter);
 app.use(logger('dev'));
 app.use(cors());
 app.use(helmet()); // helps you secure your Express apps by setting various HTTP headers
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));  // basically tells the system whether you want to use a simple algorithm for shallow parsing (i.e. false) or complex algorithm for deep parsing that can deal with nested objects (i.e. true).
 app.use(bodyParser.json()); // basically tells the system that you want json to be used
+
 
 app.listen(port, () => {
   console.log(`API Server is listening on ${port}`);
--- src/posts/actions.js
+++ src/posts/actions.js
@@ -1,1 +1,71 @@
-const url = "https://jsonplaceholder.typicode.com/posts"
+import axios from 'axios';
+import configs from '../../config/urls';
+import fs from 'fs';
+
+const urlConfig = configs['dev'];
+
+const { posts } = urlConfig;
+// const dkajsdl = urlCongig.posts;
+
+
+// complete below functions
+// function for getting one post per id as a object
+const get = async (req, res, next) => {
+  const { id }: { id: string } = req.params;
+  const { data } = await axios.get(posts);
+
+  // exercise 
+  // add way in order to gets single user per id and check does id is valid or not
+  // if user id is valid then store data into the storage file, the same as on listing
+  
+  // put your code below
+ 
+
+  
+  // put your code above 
+  await next;
+}
+
+// function for listing posts as an array of objects
+const list = async(req, res, next) => {
+  const { data } = await axios.get(posts);
+   
+  // missing this one (JSON.stringify(data, null, 2));
+  // jsonString = JSON.stringify(value, [, replacer [, space ]]);
+  // value -> the js object to conver into a JSON string
+  
+  // replacer -> a fun that alerts the behaviour of the stingification process,
+  // ... or an array of String and Number objs that serve as a whitelist for selecting
+  // ... the properties of the values obj to be included in the JSON string
+  // if this value is null or not provided, all properies of the obj are included
+  // ... in the resulting JSON string
+
+  // space -> a String or Number obj that's used to insert white space into the output
+  // ... JSON string for readability purposese.
+
+  const writeDataToStorageFile: string = JSON.stringify(data, null, 2)  
+  fs.writeFileSync('localStorage.json', writeDataToStorageFile);
+
+  res.status(200).send(data);
+  await next;
+}
+
+const create = async (req, res, next) => {
+  await next;
+}
+
+const update = async (req, res, next) => {
+  await next;
+}
+
+const del = async (req, res, next) => {
+  await next;
+};
+
+export default {
+  get,
+  list,
+  del,
+  update,
+  create,
+};
